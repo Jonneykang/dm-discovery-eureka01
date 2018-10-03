339178b933dc413825398455a85ef04c18cf1c81
JonneyKang <zhang-ning-qiang@163.com>
2018-10-03 16:30:02 +0800

    01

--- src/main/java/dm/cn/dmdiscoveryenreka01/DmDiscoveryEnreka01Application.java
+++ src/main/java/dm/cn/dmdiscoveryenreka01/DmDiscoveryEnreka01Application.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class DmDiscoveryEnreka01Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DmDiscoveryEnreka01Application.class, args);
     }
 }
